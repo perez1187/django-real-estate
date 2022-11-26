5ac60897f804b89b3c49275bd5d75315f581d1bb
perez1187 <o.perez1187@gmail.com>
2022-11-26 12:51:44 +0100

    user models.py

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,13 @@
+import uuid # Universally unique identifier
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+# local
+from .managers import CustomUserManager
+
+class User(AbstractBaseUser, PermissionsMixin):
+    pkid = models.BigAutoField(primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    username = models.CharField(verbose_name=_('Username'), max_length=255, unique=True)
